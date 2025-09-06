b22e7cf9e66a634797aeb6352b861b8eee9f686c
M-Rofi-i-Alawi <rofiialawi@gmail.com>
2025-09-06 17:06:00 +0700

    menambahkan modal dan fitur tambah data tamu

--- buku-tamu.php
+++ buku-tamu.php
@@ -10,10 +10,38 @@ include_once('templates/header.php');
     <title>Document</title>
 </head>
 <body>
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Buku Tamu</h1>
+
+    <?php
+    // jika ada tombol simpan
+    if (isset($_POST['simpan'])) {
+        if (tambah_tamu($_POST) > 0) {
+    ?>
+            <div class="alert alert-success" role="alert">
+                Data berhasil disimpan!
+            </div>
+        <?php
+        } else {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                Data gagal disimpan!
+            </div>
+    <?php
+        }
+    }
+?>
+
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Daftar Tamu</h6>
+         <button type="button" class="btn btn-primary btn-icon-split" 
+            data-toggle="modal" data-target="#tambahModal">
+            <span class="icon text-white-50">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span class="text">Data Tamu</span>
+        </button>
     </div>
     <div class="card-body">
         <div class="table-responsive">
@@ -52,11 +80,89 @@ include_once('templates/header.php');
                 </tr>
                 <?php endforeach; ?>
             </tbody>
+
         </table>
         </div>
     </div>
     </div>
 
+    <?php
+    // mengambil data tamu dengan kode terbesar
+$query = mysqli_query($koneksi,"SELECT max(id_tamu) as kodeTerbesar FROM buku_tamu");
+$data = mysqli_fetch_array($query);
+$kodeTamu = $data['kodeTerbesar'];
+
+// mengambil angka dari kode tamu terbesar
+$urutan = (int) substr($kodeTamu, 2, 3);
+
+// increment
+$urutan++;
+
+// membuat kode tamu baru
+$huruf = "zt";
+$kodeTamu = $huruf . sprintf("%03s", $urutan);
+
+    ?>
+
+    <!-- Modal Tambah -->
+    <div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tambahModalLabel">Tambah Data Tamu</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <form method="post" action="">
+                <div class="modal-body">
+                <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $kodeTamu ?>">
+                
+                <div class="form-group row">
+                    <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
+                    <div class="col-sm-8">
+                    <input type="text" class="form-control" id="nama_tamu" name="nama_tamu">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
+                    <div class="col-sm-8">
+                    <textarea class="form-control" id="alamat" name="alamat"></textarea>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="no_hp" class="col-sm-3 col-form-label">No. Telepon</label>
+                    <div class="col-sm-8">
+                    <input type="text" class="form-control" id="no_hp" name="no_hp">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="bertemu" class="col-sm-3 col-form-label">Bertemu dg.</label>
+                    <div class="col-sm-8">
+                    <input type="text" class="form-control" id="bertemu" name="bertemu">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
+                    <div class="col-sm-8">
+                    <input type="text" class="form-control" id="kepentingan" name="kepentingan">
+                    </div>
+                </div>
+                </div>
+
+                <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
+                <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
     
