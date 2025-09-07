d64d42ff9792766efc234ea4c17b298c74aafa13
M-Rofi-i-Alawi <rofiialawi@gmail.com>
2025-09-07 22:14:07 +0700

    menambahkan fitur login session dan logout

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 require('koneksi.php');
 session_start();
 
+// Jika user sudah login, langsung arahkan ke index
 if (isset($_SESSION['login'])) {
     header("Location: index.php");
     exit();
@@ -13,18 +14,20 @@ if (isset($_POST['login'])) {
 
     $result = mysqli_query($koneksi, "SELECT * FROM users WHERE username = '$username'");
 
-
-    if (mysqli_num_rows($result) === 1) {
+    if ($result && mysqli_num_rows($result) === 1) {
         $row = mysqli_fetch_assoc($result);
 
+        // verifikasi password hash
         if (password_verify($password, $row['password'])) {
             $_SESSION['login'] = true;
-            $_SESSION['username'] = $username;
+            $_SESSION['username'] = $row['username'];
             $_SESSION['role'] = $row['user_role'];
             header("Location: index.php");
             exit();
         }
     }
+
+    // jika gagal login
     $error = true;
 }
 ?>
@@ -33,37 +36,33 @@ if (isset($_POST['login'])) {
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>Login - Aplikasi Buku Tamu</title>
 
     <!-- Custom fonts for this template-->
     <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
-
 </head>
 
 <body class="bg-gradient-primary">
 
     <div class="container">
-        <?php
-        if (isset($error)): ?>
+
+        <!-- Notifikasi error -->
+        <?php if (!empty($error)) : ?>
             <div class="alert alert-danger mt-3" role="alert">
-                Username atau Password Salah!
+                Username atau Password salah!
             </div>
         <?php endif; ?>
+
         <!-- Outer Row -->
-        <div class="row justify-content-center p-8">
+        <div class="row justify-content-center">
 
             <div class="col-xl-10 col-lg-12 col-md-9">
 
@@ -76,29 +75,20 @@ if (isset($_POST['login'])) {
                                     <img src="assets/img/login-page.jpg" alt="Login Image"
                                         class="img-fluid rounded-start" style="width: 90%; height: auto;">
                                 </div>
-
                             </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
                                     </div>
                                     <form class="user" method="post" action="">
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" id="username"
-                                                name="username" placeholder="Username...">
+                                            <input type="text" class="form-control form-control-user" 
+                                                id="username" name="username" placeholder="Masukkan Username" required>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="password"
-                                                placeholder="Password..." name="password">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="remember"
-                                                    name="remember">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
+                                            <input type="password" class="form-control form-control-user" 
+                                                id="password" name="password" placeholder="Masukkan Password" required>
                                         </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block" name="login">
                                             Login
@@ -106,10 +96,10 @@ if (isset($_POST['login'])) {
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        <a class="small" href="forgot-password.html">Lupa Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="register.html">Buat Akun Baru!</a>
                                     </div>
                                 </div>
                             </div>
@@ -125,7 +115,7 @@ if (isset($_POST['login'])) {
 
     <!-- Bootstrap core JavaScript-->
     <script src="assets/vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
     <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
